92d9ea2cd9e46d6ff30182532aa6f99f58e11290
Emre Sanden <emresanden@gmail.com>
2023-11-14 13:13:23 +0100

    Fix missing character

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,5 +5,5 @@ author 'KindCoder-no <rpbanlist@sandenutvikling.no>'
 description 'Fivem resource to integrate with the RP Banlist API'
 version '1.0.0'
 
-shared_script 'config.lua
+shared_script 'config.lua'
 server_script 'server.lua'
