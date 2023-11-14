6cc48fa5a0757addb1d8f07a528cae5cd27fbe54
Emre Sanden <emresanden@gmail.com>
2023-11-14 13:12:47 +0100

    Initial setup

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'KindCoder-no <rpbanlist@sandenutvikling.no>'
+description 'Fivem resource to integrate with the RP Banlist API'
+version '1.0.0'
+
+shared_script 'config.lua
+server_script 'server.lua'
