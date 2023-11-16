7ed3734a3bd430edc00ba22c4fba19b810a53db9
Emre Sanden <emresanden@gmail.com>
2023-11-16 14:05:48 +0100

    Update comment format

--- config.lua
+++ config.lua
@@ -2,12 +2,12 @@ Config = {}
 
 Config.apikey = ""
 
-Config.autoBan = true /* If true, player will be banned automatically when they are banned on another server */
-Config.autoKick = true /* If true, player will be kicked automatically when they are banned on another server */
-Config.autoBanMessage = "You have been banned from this server!" /* Message to show when player is banned */
-Config.autoKickMessage = "You have been kicked from this server!" /* Message to show when player is kicked */
+Config.autoBan = true -- If true, player will be banned automatically when they are banned on another server 
+Config.autoKick = true -- If true, player will be kicked automatically when they are banned on another server 
+Config.autoBanMessage = "You have been banned from this server!" -- Message to show when player is banned 
+Config.autoKickMessage = "You have been kicked from this server!" -- Message to show when player is kicked 
 
-Config.discordNotifications = true /* If true, will send notifications to Discord. Webhook url is required */
-Config.discordWebhook = "" /* Discord webhook to send ban messages to */
+Config.discordNotifications = true -- If true, will send notifications to Discord. Webhook url is required 
+Config.discordWebhook = "" --  Discord webhook to send ban messages to 
 
-Config.show = true /* If true, will print debug messages to console */
+Config.show = true -- If true, will print debug messages to console 
